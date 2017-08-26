20c42ec9b00eb73fcf45893be457c204336b4c4d
John Silkey <jpsilkjr86@gmail.com>
2017-08-26 05:53:54 -0700

    added morgan logger to package.json. added starter server.js code

---
+++ server.js
@@ -0,0 +1,48 @@
+// ================ Dependencies ================
+const express = require('express'),
+	bodyParser = require('body-parser'),
+	methodOverride = require("method-override"),
+    exphbs = require('express-handlebars'),
+    logger = require('morgan'),
+    request = require('request'),
+    cheerio = require('cheerio'),
+    mongoose = require('mongoose');
+
+// sets up express app
+const app = express();
+const port = process.env.PORT || 3000;
+
+// ================ Mongoose Configuration ================
+
+
+// ================ Express Configuration ================
+// Configures Express and body parser
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+// serves public directory as static, enabling html pages to link with their assets
+app.use(express.static("public"));
+
+// Handlebars Engine Configuration
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+// Sets handlebars as rendering engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// ================ Connection Establishment ================
+// attempts to establish connection to mongoose db
+
+	// listens to port for running server
+	app.listen(port, () => {
+		console.log('App listening on port ' + port);
+		// sets up routes
+		// require('./controllers/html-routes.js')(app, db);
+		// require('./controllers/api-routes.js')(app, db);
+	});
+
+// server connection error handling
+	// console.log('Error: Failed to establish connection with MySQL.');
+	
