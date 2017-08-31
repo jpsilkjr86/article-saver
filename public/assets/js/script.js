9ef093295ee3e7a8b3a3af7ea357b80baef65b78
John Silkey <jpsilkjr86@gmail.com>
2017-08-31 16:30:04 -0700

    fixed styling of article divs for responsiveness on varying screen sizes

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -11,38 +11,45 @@ $(document).ready(function(){
 	function displayArticles (articles) {
 		for (let i = 0; i < articles.length; i++) {
 			// declare parent article element
-			let articleDiv = $('<a>').addClass('list-group-item')
-								.attr('href', articles[i].link);
+			let articleDiv = $('<a>').addClass('list-group-item');
 			// divide parent article element into three columns
 			let row = $('<div>').addClass('row').appendTo(articleDiv),
-				colLeft = $('<div>').addClass('col-xs-2').appendTo(row),
-				colMain = $('<div>').addClass('col-xs-8').appendTo(row),
-				colRight = $('<div>').addClass('col-xs-2').appendTo(row);
-			// put thumb in left column
+				colLeft = $('<div>').addClass('col-xs-3 col-md-2').appendTo(row),
+				colMain = $('<div>').addClass('col-xs-7 col-md-9').appendTo(row),
+				colRight = $('<div>').addClass('col-xs-2 col-sm-1').appendTo(row);
+			// put thumb in left column, wrapped in <a> tag
 			let thumb = $('<img>').attr('src', articles[i].thumbnail)
-							.addClass('img-responsive')
-							.appendTo(colLeft);
-			// put headline at top of right column, bolded
+							.addClass('img-responsive article-thumb');
+			$('<a>').attr('href', articles[i].link)
+					.append(thumb)
+					.appendTo(colLeft);
+			// put headline at top of right column, bolded, wrapped in <a> tag
 			let headline = $('<h4>').text(articles[i].headline)
-								.css('font-weight', 'bold')
-								.appendTo(colMain);
+								.css('font-weight', 'bold');
+			$('<a>').attr('href', articles[i].link)
+					.append(headline)
+					.appendTo(colMain);
 			// declare summary, append under headline
 			let summary = $('<p>').text(articles[i].summary).appendTo(colMain);
 			// declare byline and date next to each other
 			// let additionalInfo = $('<i>').text(articles[i].by + '|' + articles[i].date)
 			// 						.appendTo(colMain);
-			let byline = $('<div>').addClass('pull-left')
+			let byline = $('<p>').addClass('pull-left')
 							.text(articles[i].by)
 							.appendTo(colMain);
-			let date = $('<div>').addClass('pull-right')
+			let date = $('<p>').addClass('pull-right')
 							.text(articles[i].date)
 							.appendTo(colMain);
-			// add star button to the right
-			$('<span>').addClass('glyphicon glyphicon-star')
+			// add clearfix to clear floats
+			$('<div>').addClass('clearfix').appendTo(colMain);
+			// add bookmark button to the right
+			let saveBtn = $('<button>').addClass('btn btn-default');
+			$('<span>').addClass('glyphicon glyphicon-bookmark')
 					.attr('aria-hidden', 'true')
-					.appendTo(colRight);
+					.appendTo(saveBtn);
+			saveBtn.appendTo(colRight);
 			// finally, append articleDiv to the DOM
-			$('#test').prepend(articleDiv);
+			$('#articles-div').prepend(articleDiv);
 		}
 	}
 	// listener for submitting forms
