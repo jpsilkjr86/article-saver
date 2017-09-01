4246d265dceabb946e6e0153f35a03a5b32f9a38
John Silkey <jpsilkjr86@gmail.com>
2017-09-01 09:14:37 -0700

    added remote mongoose uri instead of localhost

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // ================ Dependencies ================
 const express = require('express'),
 	bodyParser = require('body-parser'),
-	methodOverride = require("method-override"),
+	methodOverride = require('method-override'),
     exphbs = require('express-handlebars'),
     logger = require('morgan'),
     request = require('request'),
@@ -14,16 +14,18 @@ const port = process.env.PORT || 3000;
 
 // ================ Mongoose Configuration ================
 // importing models
-const Article = require("./models/Article.js"),
-	Comment = require("./models/Comment.js"),
-	User = require("./models/User.js");
+const Article = require('./models/Article.js'),
+	Comment = require('./models/Comment.js'),
+	User = require('./models/User.js');
 
 // configure mongoose promises to ES6 Promises
 mongoose.Promise = Promise;
 // set database configuration
-mongoose.connect("mongodb://localhost/article-saver", {
- 	useMongoClient: true
-});
+const remoteUri = 'mongodb://heroku_21s8xm66:bl3i996k7smok7vsfbomhre6en@ds023540.mlab.com:23540/heroku_21s8xm66';
+mongoose.connect(remoteUri, { useMongoClient: true });
+// const localUri = 'mongodb://localhost/article-saver';
+// mongoose.connect(localUri, { useMongoClient: true });
+
 // save connection as variable
 const db = mongoose.connection;
 
@@ -35,24 +37,24 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride('X-HTTP-Method-Override'));
 
 // serves public directory as static, enabling html pages to link with their assets
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // Handlebars Engine Configuration
 // Override with POST having ?_method=DELETE
-app.use(methodOverride("_method"));
+app.use(methodOverride('_method'));
 // Sets handlebars as rendering engine
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
 
 // ================ Connection Establishment ================
 // show any mongoose connection errors
-db.on("error", function(error) {
- 	console.log("Mongoose Error: ", error);
+db.on('error', function(error) {
+ 	console.log('Mongoose Error: ', error);
 });
 
 // attempts to establish connection to mongoose db
-db.once("open", function() {
-	console.log("Mongoose connection successful.");
+db.once('open', function() {
+	console.log('Mongoose connection successful.');
 	// listens to port for running server within mongoose connection callback
 	app.listen(port, () => {
 		console.log('App listening on port ' + port);
