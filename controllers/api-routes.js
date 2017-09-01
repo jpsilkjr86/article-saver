eddc454f3dc3654d6e3f69e82b4ad45f246d7982
John Silkey <jpsilkjr86@gmail.com>
2017-09-01 15:04:40 -0700

    refactored api-routes to be cleaner and more logical, like the error handler

--- controllers/api-routes.js
+++ controllers/api-routes.js
@@ -2,41 +2,41 @@
 const articleSaver = require('../helpers/article-saver.js');
 // exports as function which takes in app as parameter
 module.exports = (app) => {
-	// post route for search
-	// app.get('/search/:query', (req, res) => {
+	// get route for search
 	app.get('/search', (req, res) => {
-		// graps queryStr from value of /search?q= 
-		let queryStr = req.query.q;
-		// replaces multiple spaces with just one space
-		queryStr = queryStr.replace(/\s\s+/g, ' ');
-		// replaces single spaces with plus sign
-		queryStr = queryStr.replace(/\s/g, '+');
+		// early returns if no search is specified
+		if (!req.query.q) {
+			return res.send('No search query has been specified.');
+		}
+		// graps queryStr from value of /search?q=, replacing multiple spaces with
+		// just one space and single spaces with plus signs
+		let queryStr = (req.query.q).replace(/\s\s+/g, ' ').replace(/\s/g, '+');
 		console.log('User searched: "' + queryStr + '"');
 		// sends query string to helper function searching nytimes,
 		// which returns a promise with results in the callback
 		articleSaver.nytimes.search(queryStr).then((results) => {
-			// if no results, tells user that their search did not yield results.
+			// if no results, break promise chain by throwing error message
 			if (!results || !results.length) {
-				console.log('NO RESULTS WERE FOUND FOR USER SEARCH');
-				res.json({results: [], responseMsg: "Search did not yield any results"});
 				throw 'NO RESULTS WERE FOUND FOR USER SEARCH';
-			} else {
-				console.log('RESULTS FOUND! NUMBER OF ARTICLES: ' + results.length);
-				console.log('SYNCING ARTICLES WITH THE DATABASE...');
-				// return articleSaver.db.sync to process scraped articles and return them 
-				// in a form that is synced with the mongoose database
-				return articleSaver.db.sync(results);
 			}
+			console.log('RESULTS FOUND! NUMBER OF ARTICLES: ' + results.length);
+			console.log('SYNCING ARTICLES WITH THE DATABASE...');
+			// return articleSaver.db.sync to process scraped articles and return them 
+			// in a form that is synced with the mongoose database
+			return articleSaver.db.sync(results);
 		}).then((articles) => {
 			console.log('SYNCING COMPLETE! SENDING ARTICLES TO USER.');
 			res.json({results: articles, responseMsg: "Number of Articles Found: " + articles.length});
 		}).catch((err) => {
+			// error handler, responseMsg depending on error
 			console.log(err);
-			// condition added to avoid res.send twice
-			if (err !== 'NO RESULTS WERE FOUND FOR USER SEARCH') {
-				console.log('ERROR SEARCHING WITH ARTICLE SAVER');
-				res.send('Unable to complete search.');
+			let responseMsg = '';
+			if (err === 'NO RESULTS WERE FOUND FOR USER SEARCH') {
+				responseMsg = 'User search did not yield any results.';
+			} else {
+				responseMsg = 'Unable to perform search.';
 			}
-		});
-	});
+			res.json({results: [], responseMsg: responseMsg});
+		}); // end of promise chain
+	}); // end of app.get('/search') 
 };
