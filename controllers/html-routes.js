fd9fc8cf6ef5046a2dd6b194354c9ef43e24248b
John Silkey <jpsilkjr86@gmail.com>
2017-09-01 23:18:25 -0700

    added signin page with starter styling

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,6 +1,6 @@
 // exports html-routes as function which takes in app paramater
 module.exports = (app) => {
-
+	// route for index. if not signed in, redirect to signin page
 	app.get('/', (req, res) => {
 		if (req.user) {
 			console.log('Logged in as user ' + req.user.username + '');
@@ -8,6 +8,15 @@ module.exports = (app) => {
 		} else {
 			console.log('No logged-in user found. Redirecting to signin page...');
 			res.redirect('/signin');
+		}
+	});
+	// route for signin page. if signed in, redirect to index
+	app.get('/signin', (req, res) => {
+		if (req.user) {
+			console.log('Logged in as user ' + req.user.username + '');
+			res.redirect('/');
+		} else {
+			res.render('signin');
 		}
 	});
 };
