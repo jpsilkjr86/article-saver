0d9001a3922b8cd43fbd5b50874b15a8dd237841
John Silkey <jpsilkjr86@gmail.com>
2017-09-01 22:19:28 -0700

    added starter code for passport, session, bcrypt, cookieparser. tweeked User model

--- models/User.js
+++ models/User.js
@@ -5,10 +5,11 @@ var Schema = mongoose.Schema;
 
 // Create User schema
 var UserSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: String,
-  email: { type: String, required: true, unique: true },
+  username: {type: String, required: true, unique: true},
   password: { type: String, required: true },
+  email: { type: String, required: true },
+  first_name: { type: String, required: true },
+  last_name: String,
   created_at: { type: Date, default: Date.now },
   // references user's posted comments through Comment model
   posted_comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
--- server.js
+++ server.js
@@ -6,7 +6,12 @@ const express = require('express'),
     logger = require('morgan'),
     request = require('request'),
     cheerio = require('cheerio'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    cookieParser = require('cookie-parser'),
+    session = require('express-session'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
+    bcrypt = require('bcryptjs'); // for encryption;
 
 // sets up express app
 const app = express();
@@ -23,6 +28,107 @@ mongoose.connect(remoteUri, { useMongoClient: true });
 
 // save connection as variable
 const db = mongoose.connection;
+
+// importing database models (mongoose)
+const Article = require('./models/Article.js'),
+	Comment = require('./models/Comment.js'),
+	User = require('./models/User.js');
+
+// ================ Passport Configuration (User Authentication) ================
+// Passport session setup
+passport.serializeUser((user, done) => {
+	console.log("serializing " + user.username);
+	done(null, user);
+});
+passport.deserializeUser((obj, done) => {
+	console.log("deserializing " + obj);
+	done(null, obj);
+});
+// sets up sign-in LocalStrategy within Passport
+passport.use('local-signin', new LocalStrategy({
+    passReqToCallback : true //allows us to pass back the request to the callback
+	}, (req, username, password, done) => {
+		// checks database for user by username
+		User.where({'username' : username}).findOne((err, user) => {
+			if (err) {
+				// sends and returns error if database throws an error
+				console.log("SERVER ERROR");
+				req.session.error = 'Server error.';
+				return done(err);
+			}
+			// if no result is found, return done(null, false), set session msg
+			if (user == null) {
+				console.log("USER NOT FOUND:", username);
+				req.session.notice = 'No user exists with that username.'
+								+ ' Please try again or create a new account.';
+				return done(null, false);
+			}
+			console.log("FOUND USER: " + username);
+			// saves encrypted password as locally scoped variable
+			const hash = user.password;
+			// uses bcrypt to see if password matches hash. if so, returns done(null, user)
+			if (bcrypt.compareSync(password, hash)) {
+				console.log("PASSWORD MATCHED!");
+				console.log("LOGGED IN AS: " + username);
+				req.session.success = 'You are successfully logged in as ' + username + '!';
+				return done(null, user);
+			}
+			// if strategy reaches this point, it must mean the password doesn't match
+			console.log("PASSWORD DOESN'T MATCH");
+			req.session.notice = 'Password is incorrect. Please try again.';
+			return done(null, false);
+		}); // end of .findOne callback
+	} //end of passport callback
+)); // end of passport.use
+// Sets up sign-up LocalStrategy within Passport
+passport.use('local-signup', new LocalStrategy({
+	passReqToCallback : true //allows us to pass back the request to the callback
+	}, (req, username, password, done) => {
+		// first searches to see if username exists in database
+		User.where({'username' : username}).findOne((err, user) => {
+			// early returns if there's an error
+			if (err) {
+				console.log(err);
+				console.log('FAILED TO CREATE USER:', username);
+				req.session.error = 'Server error: Failed to create new user account.'
+									+ ' Please try again later.';
+				return done(err);
+			}				
+			// if user exists, display fail messages and return done(null, false)
+			if (user != null) {
+				console.log("USER ALREADY EXISTS:" + username);
+				req.session.notice = "An account has already been created with"
+								+ " that username. Please try another one."
+				return done(null, false);
+			}
+			// instantiates locally scoped constables, encrypts password argument.
+			// values come from username parameter, hash (encrypted pw), and req.body
+			const hash = bcrypt.hashSync(password, 8);
+			const newUser = {
+				username: username,
+				password: hash,
+				email: req.body.email,
+				first_name: req.body.first_name,
+				last_name: req.body.last_name
+			};
+			console.log("CREATING USER: " + username + "...");
+			// attempts to insert new user into database.
+			User.create(newUser).then(result => {
+				// if user was successfully able to create an account, display success
+				// message and return done(null, user)
+				console.log('ACCOUNT SUCCESSFULLY CREATED! ' + username);
+				req.session.success = "Account successfully created! Signed in as: " + username;
+				return done(null, newUser);
+			}).catch(err => {
+				console.log('ERROR. FAILED TO CREATE USER.');
+				console.log(err);
+				req.session.error = 'Failed to create user.';
+				return done(err);
+			});
+		// if an error was thrown then display server error messages
+		}); // end of .findOne callback
+	} // end of passport callback
+)); // end of passport.use
 
 // ================ Express Configuration ================
 // Configures Express and body parser
@@ -41,6 +147,29 @@ app.use(methodOverride('_method'));
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+// Passport, Session and cookieParser configuration with express instance
+app.use(cookieParser());
+app.use(session({secret: 'plainbozo', saveUninitialized: true, resave: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Session-persisted message middleware
+app.use((req, res, next) => {
+  var err = req.session.error,
+      msg = req.session.notice,
+      success = req.session.success;
+
+  delete req.session.error;
+  delete req.session.success;
+  delete req.session.notice;
+
+  if (err) res.locals.error = err;
+  if (msg) res.locals.notice = msg;
+  if (success) res.locals.success = success;
+
+  next();
+});
+
 // ================ Connection Establishment ================
 // show any mongoose connection errors
 db.on('error', function(error) {
@@ -55,6 +184,6 @@ db.once('open', function() {
 		console.log('App listening on port ' + port);
 		// sets up routes
 		require('./controllers/html-routes.js')(app);
-		require('./controllers/api-routes.js')(app);
+		require('./controllers/api-routes.js')(app, passport);
 	});
 });
